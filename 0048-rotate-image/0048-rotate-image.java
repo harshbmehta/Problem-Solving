a61ce247311afb489002c96b58f5ab23dbf4a416
harshbmehta <65001929+harshbmehta@users.noreply.github.com>
2023-03-18 10:21:02 -0700

    Time: 0 ms (100.00%), Space: 42.9 MB (13.74%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,35 @@
+/*
+Let M be the number of cells in the matrix.
+
+Time complexity : O(M), as each cell is getting read once and written once.
+
+Space complexity : O(1) because we do not use any other additional data structures.
+
+*/
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        int left = 0; 
+        int right = n-1;
+   
+        while(left<right) {
+            for(int i=0; i<right-left;i++) { // ****** Right-Left
+                int top = left;
+                int bottom=right;
+                int temp = matrix[top][left+i];
+
+                matrix[top][left+i] = matrix[bottom-i][left];
+
+                matrix[bottom-i][left] = matrix[bottom][right-i];
+
+                matrix[bottom][right-i] = matrix[top+i][right];
+
+                matrix[top+i][right] = temp;
+            }
+            // remember
+            left++;
+            right--;
+        }
+    }
+}
