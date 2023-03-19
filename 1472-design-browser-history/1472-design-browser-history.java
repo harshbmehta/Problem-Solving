3dc4d917768f6cf44a657324ce4d1d08ed18d0e4
harshbmehta <65001929+harshbmehta@users.noreply.github.com>
2023-03-19 12:08:26 -0700

    Time: 112 ms (17.19%), Space: 50.9 MB (13.30%) - LeetHub

---
+++ 1472-design-browser-history/1472-design-browser-history.java
@@ -0,0 +1,42 @@
+class BrowserHistory {
+    Stack<String> history, forward;
+    String currUrl;
+
+    public BrowserHistory(String homepage) {
+        history = new Stack<>();
+        forward = new Stack<>();
+        currUrl = homepage;
+    }
+    
+    public void visit(String url) {
+        history.push(currUrl);
+        forward.clear();
+        currUrl = url;
+    }
+    
+    public String back(int steps) {
+        while(steps > 0 && !history.isEmpty()) {
+                forward.push(currUrl);
+                currUrl = history.pop();
+                steps--;
+        }
+        return currUrl;      
+    }
+    
+    public String forward(int steps) {
+        while(steps > 0 && !forward.isEmpty()) {
+                history.push(currUrl);
+                currUrl = forward.pop();
+                steps--;
+        }
+        return currUrl;
+    }
+}
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory obj = new BrowserHistory(homepage);
+ * obj.visit(url);
+ * String param_2 = obj.back(steps);
+ * String param_3 = obj.forward(steps);
+ */
