e8d52572a53f8feff611c14945fc5b611da498c2
harshbmehta <65001929+harshbmehta@users.noreply.github.com>
2023-03-19 12:11:37 -0700

    Time: 59 ms (55.63%), Space: 50.7 MB (30.44%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.java
+++ 1472-design-browser-history/1472-design-browser-history.java
@@ -1,41 +1,77 @@
-
-/*
-Using two-stacks
-*/
 class BrowserHistory {
-    Stack<String> history, forward;
-    String currUrl;
+    ArrayList<String> visitedURLs;
+    int currURL, lastURL;
 
     public BrowserHistory(String homepage) {
-        history = new Stack<>();
-        forward = new Stack<>();
-        currUrl = homepage;
+        // 'homepage' is the first visited URL.
+        visitedURLs = new ArrayList<String>(Arrays.asList(homepage));
+        currURL = 0; 
+        lastURL = 0;
     }
     
     public void visit(String url) {
-        history.push(currUrl);
-        forward.clear();
-        currUrl = url;
+        currURL += 1;
+        if (visitedURLs.size() > currURL) {
+            // We have enough space in our array to overwrite an old 'url' entry with new one.
+            visitedURLs.set(currURL, url);
+        } else {
+            // We have to insert a new 'url' entry at the end.
+            visitedURLs.add(url);
+        }
+        // This 'url' will be last URL if we try to go forward.
+        lastURL = currURL;
     }
     
     public String back(int steps) {
-        while(steps > 0 && !history.isEmpty()) {
-                forward.push(currUrl);
-                currUrl = history.pop();
-                steps--;
-        }
-        return currUrl;      
+        // Move 'currURL' pointer in left direction.
+        currURL = Math.max(0, currURL - steps);
+        return visitedURLs.get(currURL);
     }
     
     public String forward(int steps) {
-        while(steps > 0 && !forward.isEmpty()) {
-                history.push(currUrl);
-                currUrl = forward.pop();
-                steps--;
-        }
-        return currUrl;
+        // Move 'currURL' pointer in right direction.
+        currURL = Math.min(lastURL, currURL + steps);
+        return visitedURLs.get(currURL);
     }
 }
+/*
+Using two-stacks
+*/
+// Slower runtime compared to Dynamic array approach
+// class BrowserHistory {
+//     Stack<String> history, forward;
+//     String currUrl;
+
+//     public BrowserHistory(String homepage) {
+//         history = new Stack<>();
+//         forward = new Stack<>();
+//         currUrl = homepage;
+//     }
+    
+//     public void visit(String url) {
+//         history.push(currUrl);
+//         forward.clear();
+//         currUrl = url;
+//     }
+    
+//     public String back(int steps) {
+//         while(steps > 0 && !history.isEmpty()) {
+//                 forward.push(currUrl);
+//                 currUrl = history.pop();
+//                 steps--;
+//         }
+//         return currUrl;      
+//     }
+    
+//     public String forward(int steps) {
+//         while(steps > 0 && !forward.isEmpty()) {
+//                 history.push(currUrl);
+//                 currUrl = forward.pop();
+//                 steps--;
+//         }
+//         return currUrl;
+//     }
+// }
 
 /**
  * Your BrowserHistory object will be instantiated and called as such:
