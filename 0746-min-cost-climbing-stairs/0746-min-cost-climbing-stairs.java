bfe196b9f708a1580e1df911b906339f448267e5
harshbmehta <65001929+harshbmehta@users.noreply.github.com>
2023-03-19 15:09:26 -0700

    Time: 1 ms (51.27%), Space: 42.1 MB (58.41%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,3 +1,5 @@
+// Solution frm https://www.youtube.com/watch?v=ktmzAZWkEZ0
+
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
         for (int i = cost.length-3; i >= 0; i--) {
