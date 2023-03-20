4fc0b87c2cd6345bb2499550a1c45ca3c770509d
harshbmehta <65001929+harshbmehta@users.noreply.github.com>
2023-03-20 09:10:51 -0700

    Time: 2 ms (96.79%), Space: 42.9 MB (27.72%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -17,6 +17,7 @@ class Solution {
         ListNode dummyHead = new ListNode(0);
         ListNode curr = dummyHead;
         int carry = 0;
+        //***remember edge conditions and if conditions
         while (l1 != null || l2 != null || carry != 0) {
             int x = (l1 != null) ? l1.val : 0;
             int y = (l2 != null) ? l2.val : 0;
