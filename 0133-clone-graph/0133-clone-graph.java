627d6ad0b55f0e8455385f9cf02c3fa308b9e1c4
harshbmehta <65001929+harshbmehta@users.noreply.github.com>
2023-03-20 11:38:39 -0700

    Time: 26 ms (49.52%), Space: 42.5 MB (51.91%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.java
@@ -0,0 +1,45 @@
+/*
+Complexity Analysis
+
+Time Complexity: O(N+M)O(N + M)O(N+M), where NNN is a number of nodes (vertices) and MMM is a number of edges.
+Space Complexity: O(N). This space is occupied by the visited hash map and in addition to that, space would also be occupied by the recursion stack since we are adopting a recursive approach here. The space occupied by the recursion stack would be equal to O(H) where H is the height of the graph. Overall, the space complexity would be O(N). 
+*/
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    Map<Integer, Node> map = new HashMap<>();
+    public Node cloneGraph(Node node) {
+        if(node == null) return null;
+
+        if(map.containsKey(node.val)) {
+            return map.get(node.val);
+        }
+
+        Node newNode = new Node(node.val);
+        map.put(node.val, newNode);
+
+        for(Node neighbor: node.neighbors) {
+            newNode.neighbors.add(cloneGraph(neighbor));
+        }
+        return newNode;
+    }
+}
