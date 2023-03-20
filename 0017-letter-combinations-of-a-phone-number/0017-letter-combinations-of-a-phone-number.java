08a17a79b1602055415a10f5eb1929e51e611082
harshbmehta <65001929+harshbmehta@users.noreply.github.com>
2023-03-20 11:30:32 -0700

    Time: 5 ms (45.49%), Space: 42.9 MB (10.92%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    List<String> res = new ArrayList<>();
+    String[] KEYS = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    public List<String> letterCombinations(String digits) {
+        if(digits.length() == 0) return new ArrayList<>();
+        dfs(0, digits, "");
+        return res;
+    }
+
+
+    private void dfs(int idx, String digits, String prefix) {
+        if(idx == digits.length()) {
+            res.add(prefix);
+            return;
+        } else {
+            String ltrs = KEYS[digits.charAt(idx) - '0'];
+            for(int i=0; i<ltrs.length();i++) {
+                dfs(idx+1, digits, prefix+ltrs.charAt(i));
+            }
+        }
+    }
+}
