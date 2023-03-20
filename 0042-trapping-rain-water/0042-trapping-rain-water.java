654cea58717e2e708076e9d81b33b574f4807171
harshbmehta <65001929+harshbmehta@users.noreply.github.com>
2023-03-20 00:07:03 -0700

    Time: 1 ms (98.64%), Space: 43.7 MB (16.09%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int trap(int[] height) {
+        int result = 0;
+        int start = 0;
+        int end = height.length - 1;
+        while (start < end) {
+            if (height[start] <= height[end]) {
+                int current = height[start];
+                while (height[++start] < current) {
+                    result += current - height[start];
+                }
+            } else {
+                int current = height[end];
+                while(height[--end] < current) {
+                    result += current - height[end];
+                }
+            }
+        }
+        return result;
+    }
+}
