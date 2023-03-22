dd7f087d476512c76efbbdd8d7b7ced902af5226
harshbmehta <65001929+harshbmehta@users.noreply.github.com>
2023-03-21 18:11:36 -0700

    Time: 9 ms (33.85%), Space: 43.7 MB (14.65%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.java
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.java
@@ -1,24 +1,30 @@
 class Solution {
     public List<String> findItinerary(List<List<String>> tickets) {
-        LinkedList<String> itinerary = new LinkedList<>();
-        Map<String, PriorityQueue<String>> visits = new HashMap<>();
-
-        for(List<String> ticket: tickets) {
-            visits.computeIfAbsent(ticket.get(0), k -> new PriorityQueue<>())
-                .offer(ticket.get(1));
+        LinkedList<String> itinerary = new LinkedList<>(); // to return the result
+        Map<String, PriorityQueue<String>> visits = new HashMap<>(); // create map of to and from while maintaining lexical order using priority queue
+        
+        // create map of source -> priority queue
+        for(List<String> airports: tickets) {
+            String source = airports.get(0); // source
+            String dest = airports.get(1);
+            
+            visits.computeIfAbsent(source, k -> new PriorityQueue<>())
+                .offer(dest);
         }
-
-        Stack<String> tripOrder = new Stack<>();
-        tripOrder.push("JFK");
-
-        while(!tripOrder.isEmpty()) {
-            String destination = tripOrder.peek();
-            if(visits.getOrDefault(destination, new PriorityQueue()).isEmpty()) {
-                itinerary.addFirst(tripOrder.pop());
+        
+        // Create the stack to keep the track of all the airports
+        Stack<String> airportStk = new Stack<>();
+        // start airport is JFK
+        airportStk.push("JFK");
+        
+        while(!airportStk.isEmpty()) {
+            String startLoc = airportStk.peek();
+            if(visits.getOrDefault(startLoc, new PriorityQueue()).isEmpty()) {
+                itinerary.addFirst(airportStk.pop());
             } else {
-                tripOrder.push(visits.get(destination).poll());
+                airportStk.push(visits.get(startLoc).poll());
             }
-        }
-        return itinerary;
+        }        
+        return itinerary;    
     }
 }
