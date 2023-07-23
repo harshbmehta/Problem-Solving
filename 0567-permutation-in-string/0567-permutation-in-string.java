0447a00ce14c7aff4cd6636dcd346a5caa129a23
harshbmehta <65001929+harshbmehta@users.noreply.github.com>
2023-07-23 14:24:24 -0700

    Time: 180 ms (20.97%), Space: 44.5 MB (5.82%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -6,6 +6,9 @@ class Solution {
         for (int i = 0; i < s1.length(); i++)
             s1map[s1.charAt(i) - 'a']++;
         for (int i = 0; i <= s2.length() - s1.length(); i++) {
+//             size of window will be length of s1 and hence number of windows will be s2-s1+1
+// eg. s1="ab", s2="bvcsxba", windows => bv vc cs sx xb ba
+// for each window create seperate new s2 map and match with s1
             int[] s2map = new int[26];
             for (int j = 0; j < s1.length(); j++) {
                 s2map[s2.charAt(i + j) - 'a']++;
