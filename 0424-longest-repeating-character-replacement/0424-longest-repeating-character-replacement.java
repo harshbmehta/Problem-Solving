2830a2b0b637b08c31a16f389e1a70d5a57083a0
harshbmehta <65001929+harshbmehta@users.noreply.github.com>
2023-08-08 22:22:19 -0700

    Time: 9 ms (68.35%), Space: 41.3 MB (92.79%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -2,7 +2,7 @@
 
 /* Time: O(n): In this approach, we access each index of the string at most two times. When it is added to the sliding window, and when it is removed from the sliding window. The sliding window always moves forward. In each step, we update the frequency map, maxFrequency, and check for validity, they are all constant-time operations. To sum up, the time complexity is proportional to the number of characters in the string - O(n).
 
-Space: O(1): Similar to the previous approaches, this approach requires an auxiliary frequency map. The maximum number of keys in the map equals the number of unique characters in the string. If there are mmm unique characters, then the memory required is proportional to mmm. So the space complexity is O(m). Considering uppercase English letters only, m=26.
+Space: O(1): Similar to the previous approaches, this approach requires an auxiliary frequency map. The maximum number of keys in the map equals the number of unique characters in the string. If there are mmm unique characters, then the memory required is proportional to m. So the space complexity is O(m). Considering uppercase English letters only, m=26.
 */
 
 class Solution {
