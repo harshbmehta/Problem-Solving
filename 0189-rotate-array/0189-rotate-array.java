a01e31baaee03529279d18ee9e1ab8bbd710f10e
harshbmehta <65001929+harshbmehta@users.noreply.github.com>
2023-09-16 13:30:56 -0700

    Time: 0 ms (100.00%), Space: 56 MB (33.97%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,26 @@
+/*
+Complexity Analysis
+
+Time complexity: O(n). n elements are reversed a total of three times.
+
+Space complexity: O(1). No extra space is used.
+*/
+
+
+class Solution {
+  public void rotate(int[] nums, int k) {
+    k %= nums.length;
+    reverse(nums, 0, nums.length - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, nums.length - 1);
+  }
+  public void reverse(int[] nums, int start, int end) {
+    while (start < end) {
+      int temp = nums[start];
+      nums[start] = nums[end];
+      nums[end] = temp;
+      start++;
+      end--;
+    }
+  }
+}
