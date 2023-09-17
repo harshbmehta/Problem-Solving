d0b5f6be846319fbb01209c4ca55b145209f67ae
harshbmehta <65001929+harshbmehta@users.noreply.github.com>
2023-09-17 13:16:01 -0700

    Time: 17 ms (5.99%), Space: 43.8 MB (6.67%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -1,9 +1,23 @@
+// Recursive Approach
+// Complexity Analysis
+
+// Time complexity : O(n+m)
+
+// Because each recursive call increments the pointer to l1 or l2 by one (approaching the dangling null at the end of each list), there will be exactly one call to mergeTwoLists per element in each list. Therefore, the time complexity is linear in the combined size of the lists.
+
+// Space complexity : O(n+m)
+
+// The first call to mergeTwoLists does not return until the ends of both l1 and l2 have been reached, so n+m stack frames consume O(n+m) space.
+
 class Solution {
 
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+//         termination condition
         if (list1 == null) return list2;
         if (list2 == null) return list1;
-
+        
+        
+        System.out.println(list1.val + ", " + list2.val);
         if (list1.val <= list2.val) {
             list1.next = mergeTwoLists(list1.next, list2);
             return list1;
@@ -13,3 +27,45 @@ class Solution {
         }
     }
 }
+
+
+// Iterative Approach
+
+// Complexity Analysis
+
+// Time complexity : O(n+m)
+
+// Because exactly one of l1 and l2 is incremented on each loop
+// iteration, the while loop runs for a number of iterations equal to the
+// sum of the lengths of the two lists. All other work is constant, so the
+// overall complexity is linear.
+
+// Space complexity : O(1)
+
+// The iterative approach only allocates a few pointers, so it has a
+// constant overall memory footprint.
+
+// class Solution {
+//     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+//         // maintain an unchanging reference to node ahead of the return node.
+//         ListNode prehead = new ListNode(-1);
+
+//         ListNode prev = prehead;
+//         while (l1 != null && l2 != null) {
+//             if (l1.val <= l2.val) {
+//                 prev.next = l1;
+//                 l1 = l1.next;
+//             } else {
+//                 prev.next = l2;
+//                 l2 = l2.next;
+//             }
+//             prev = prev.next;
+//         }
+
+//         // At least one of l1 and l2 can still have nodes at this point, so connect
+//         // the non-null list to the end of the merged list.
+//         prev.next = l1 == null ? l2 : l1;
+
+//         return prehead.next;
+//     }
+// }
