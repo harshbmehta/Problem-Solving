592b8fb87d1a46c596611fd35c119833aa0b0080
harshbmehta <65001929+harshbmehta@users.noreply.github.com>
2023-09-17 21:53:36 -0700

    Time: 4 ms (74.68%), Space: 44 MB (20.49%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+// Time complexity : O(Nlog⁡k) where k is the number of linked lists.
+
+// The comparison cost will be reduced to O(log⁡k) for every pop and insertion to priority queue. But finding the node with the smallest value just costs O(1) time.
+// There are N nodes in the final linked list.
+
+// Space complexity :
+
+// O(n) Creating a new linked list costs O(n) space.
+// O(k) The code above present applies in-place method which cost O(1) space. And the priority queue (often implemented with heaps) costs O(k) space (it's far less than N in most situations).
+
+
+class Solution {
+  public ListNode mergeKLists(ListNode[] lists) {
+    ListNode dummy = new ListNode(0);
+    ListNode curr = dummy;
+    Queue<ListNode> minHeap = new PriorityQueue<>((a, b) -> a.val - b.val);
+
+    for (final ListNode list : lists)
+      if (list != null)
+        minHeap.offer(list);
+
+    while (!minHeap.isEmpty()) {
+      ListNode minNode = minHeap.poll();
+      if (minNode.next != null)
+        minHeap.offer(minNode.next);
+      curr.next = minNode;
+      curr = minNode;
+    }
+
+    return dummy.next;
+  }
+}
