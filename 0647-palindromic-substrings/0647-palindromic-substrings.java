30b4230587f9d390a21d5df29e4377deb21608e4
harshbmehta <65001929+harshbmehta@users.noreply.github.com>
2023-10-25 21:43:15 -0700

    Time: 2 ms (94.80%), Space: 40 MB (95.51%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,22 +1,26 @@
 class Solution {
     public int countSubstrings(String s) {
-    if(s == null || s.length() == 0) return 0;
-    int count = 0;
-    for(int i = 0; i<s.length();i++) {
-        count += extractPalindrome(s, i, i); // odd
-        count += extractPalindrome(s, i, i+1); //even
+        if(s.length() == 0) {
+            return 0;
+        }
+        
+        int count=0;
+        for(int i=0; i<s.length();i++) {
+            count += extractPalindrome(s, i, i);
+            count += extractPalindrome(s, i, i+1);
+        }
+        
+        return count;
     }
-
-    return count;
-    }
-
-    private int extractPalindrome(String str, int left, int right) {
+    
+    private int extractPalindrome(String s, int i, int j) {
         int count = 0;
-        while(left>=0 && right<str.length() && str.charAt(left) == str.charAt(right)) {
-            left --;
-            right++;
+        while(i>=0 && j<s.length() && s.charAt(i) == s.charAt(j)) {
+            i--;
+            j++;
             count++;
         }
+        
         return count;
     }
 }
