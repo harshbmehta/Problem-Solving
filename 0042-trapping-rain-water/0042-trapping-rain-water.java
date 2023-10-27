f7a3cda03b3acfe8545dc0f2bbb86baede9054c5
harshbmehta <65001929+harshbmehta@users.noreply.github.com>
2023-10-26 22:07:52 -0700

    Time: 1 ms (75.67%), Space: 44.2 MB (62.11%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,21 +1,46 @@
+// class Solution {
+//     public int trap(int[] height) {
+//         int result = 0;
+//         int start = 0;
+//         int end = height.length - 1;
+//         while (start < end) {
+//             if (height[start] <= height[end]) {
+//                 int current = height[start];
+//                 while (height[++start] < current) {
+//                     result += current - height[start];
+//                 }
+//             } else {
+//                 int current = height[end];
+//                 while(height[--end] < current) {
+//                     result += current - height[end];
+//                 }
+//             }
+//         }
+//         return result;
+//     }
+// }
 class Solution {
-    public int trap(int[] height) {
-        int result = 0;
-        int start = 0;
-        int end = height.length - 1;
-        while (start < end) {
-            if (height[start] <= height[end]) {
-                int current = height[start];
-                while (height[++start] < current) {
-                    result += current - height[start];
-                }
+
+    public int trap(int[] heights) {
+        if (heights.length == 0) return 0;
+
+        int l = 0, r = heights.length - 1;
+        int leftMax = heights[l], rightMax = heights[r];
+        int res = 0;
+
+        while (l < r) {
+            if (leftMax < rightMax) {
+                l++;
+                leftMax = Math.max(leftMax, heights[l]);
+                res += leftMax - heights[l];
             } else {
-                int current = height[end];
-                while(height[--end] < current) {
-                    result += current - height[end];
-                }
+                r--;
+                rightMax = Math.max(rightMax, heights[r]);
+                res += rightMax - heights[r];
             }
         }
-        return result;
+
+        return res;
     }
 }
+
