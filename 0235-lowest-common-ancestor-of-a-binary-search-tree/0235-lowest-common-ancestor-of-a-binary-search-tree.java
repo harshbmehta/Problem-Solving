f77d03f641ba91da31a51b75bc3acd9ba5a13354
harshbmehta <65001929+harshbmehta@users.noreply.github.com>
2023-10-29 18:41:07 -0700

    Time: 5 ms (100.00%), Space: 44.7 MB (5.62%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -1,6 +1,3 @@
-/*
-Time: O(h)
-Space: O(h)
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -12,31 +9,15 @@ Space: O(h)
  */
 
 class Solution {
-    private TreeNode ans;
-
-    public Solution() {
-        // Variable to store LCA node.
-        this.ans = null;
-    }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        this.recurseTree(root, p, q);
-        return this.ans;
-    }
-
-    public boolean recurseTree(TreeNode current, TreeNode p, TreeNode q) {
-        if(current == null) {
-            return false;
+        while(root.val != p.val && root.val != q.val){
+            if(root.val > p.val && root.val > q.val)
+                root = root.left;
+            else if(root.val < p.val && root.val < q.val)
+                root = root.right;
+            else
+                break;
         }
-
-        int left = recurseTree(current.left, p, q)? 1: 0;
-        int right = recurseTree(current.right, p, q)? 1: 0;
-
-        int mid = (current == p || current == q)? 1: 0;
-
-        if(mid+left+right>=2) {
-            this.ans = current;
-        }
-
-        return (mid+left+right>0);
+        return root;
     }
 }
